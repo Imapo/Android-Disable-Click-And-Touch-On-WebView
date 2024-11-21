9a0fd84c8d4a40a16220fd879ef7df4fa4d4e09d
Imapo <83175739+Imapo@users.noreply.github.com>
2024-11-21 10:37:47 +0500

    Update ManiActivity.kt

--- ManiActivity.kt
+++ ManiActivity.kt
@@ -1,1 +1,26 @@
+package com.example.disableclickonwebview
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        // Настройка WebView
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/sample.html")
+        webView.isClickable = false
+        webView.isFocusable = false
+        webView.setOnTouchListener { _, _ -> true }
+    }
+}
